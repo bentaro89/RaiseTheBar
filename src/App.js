5f04dc518513ee3dfd4c90b1cff9184b01603091
camdenhao <camden.hao@gmail.com>
2021-04-17 12:35:53 -0400

    STT Api

--- src/App.js
+++ src/App.js
@@ -7,15 +7,30 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      test1: "test it!"
+      test1: "test it!",
+      apiResponse: "",
     }
   }
+  //Repeatedly call API to obtain most recent microphone input 
+  async componentDidMount() {
+    try {
+      setInterval(async () => {
+        
+        fetch("http://localhost:9000/STTApi")
+          .then(res => res.text())
+            .then(res => this.setState({apiResponse: res}));
+      }, 50);
+    } catch(e) {
+      console.log(e);
+    }
+}
   render (){
     db.addDog(this.state.test1); // Test for database!
 
     return(
     <div className="App">
     <header className="App-header">
+      <p>{this.state.apiResponse}</p>
       <img src={logo} className="App-logo" alt="logo" />
       <p>
         Edit <code>src/App.js</code> and save to reload.
