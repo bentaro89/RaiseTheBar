bca600caceffff803013b40964a2642efab9b5cc
Adrienne Ko <koadrienne@adriennes-mbp.lan>
2021-04-17 21:21:16 -0400

    Fix rap page

--- src/components/Rap/AudioPlayer.js
+++ src/components/Rap/AudioPlayer.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import ReactAudioPlayer from 'react-audio-player'
 
 import mp3_file from '../../audio/test.mp3'
+
 class AudioPlayer extends Component  {
     render() {
         return (
