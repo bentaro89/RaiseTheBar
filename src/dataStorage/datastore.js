5ceb84d51a0b6c49d3476030aba8d88381db6f42
Benedict Tedjokusumo <bentaro89@gmail.com>
2021-04-18 03:29:08 -0400

    added comments for database

--- src/dataStorage/datastore.js
+++ src/dataStorage/datastore.js
@@ -4,12 +4,14 @@ const config = require('./key.json')
 firebase.initializeApp(config);
 const database = firebase.database();
 
+// Add a name and score to the database
 export function addScore(name, score) {
   database.ref('Scores/').push({
     name, score
   });
 }
 
+// Returns all names and scores
 export function fetchScores(callback){
   database.ref('Scores/').on('value', (snapshot) => {
     callback(snapshot.val());
